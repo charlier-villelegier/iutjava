83bf21b3d7bcbdc9defc0841581df51b0e3817dc
charlier-villelegier <xaviervillelegier@client-172-18-67-166.clients.u-psud.fr>
2015-11-17 10:31:29 +0100

    CommandLineOption

--- workspace/iuttp/src/edu/iut/app/CommandLineOption.java
+++ workspace/iuttp/src/edu/iut/app/CommandLineOption.java
@@ -1,6 +1,6 @@
 package edu.iut.app;
 
-public class CommandLineOption /* EX1 : CLASSE GENERIC SUR le type ValueType */ {
+public class CommandLineOption<ValueType> {
 	
 	public enum OptionType{
 		NONE("None"),
@@ -20,14 +20,26 @@ public class CommandLineOption /* EX1 : CLASSE GENERIC SUR le type ValueType */ 
 		}		
 	}
 	
-	public CommandLineOption() {		
-		/* EX1: Initializer les attributs */
+	public CommandLineOption() {	
+		this.key=new String();
+		this.description=new String();
+		this.defaultValue=null;
+		this.value=null;
+		this.optionType=OptionType.NONE;
+		
 	}
 	public CommandLineOption(final OptionType optionType, final String key, final String description, final ValueType defaultValue) {
-		/* EX1 : Affecter les attributs */
+		this.optionType=optionType;
+		this.key=key;
+		this.description=description;
+		this.defaultValue=defaultValue;
+		this.value=null;
 	}
 	public void setOption(OptionType optionType, String key, String description, ValueType defaultValue) {
-		/* EX1 : Affecter les attributs */
+		this.optionType=optionType;
+		this.key=key;
+		this.description=description;
+		this.defaultValue=defaultValue;
 	}
 	public  void setValue(ValueType value) {
 		this.value = value;
